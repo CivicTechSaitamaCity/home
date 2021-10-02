beff4c9724fd6f99bc34777390f915f04e419f4e
sizka9 <sizka9@users.noreply.github.com>
2021-10-02 05:30:27 +0000

    deploy: f4237ded01ad81a3881c0fe501630b55347d3061

---
+++ _nuxt/static/1633152626/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1633152626",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"232da8e3"}}}}(null,"\u002F"));
