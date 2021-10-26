98ec46e1eb8b2281dbab99ea9edd89c1b78f5779
sizka9 <sizka9@users.noreply.github.com>
2021-10-26 02:30:27 +0000

    deploy: 4dcd908b368b46d5747fac34a94b47f6efb5f4ab

--- _nuxt/static/1635141327/report/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1635141327",layout:"default",error:a,serverRendered:true,routePath:"\u002Freport",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"70a034a3"}}}}(null));
