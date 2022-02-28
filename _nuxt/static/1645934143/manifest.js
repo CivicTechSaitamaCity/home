480d1aad1343d9f64036f2e0dbab15b7f5a80132
otkzh <otkzh@users.noreply.github.com>
2022-02-28 13:40:43 +0000

    deploy: 11dfcd6ff02fb84858b338077261edee9cf0a230

--- _nuxt/static/1645934143/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Freport","\u002Freport\u002F20211025","\u002Freport\u002F20210728","\u002Freport\u002F20210630","\u002Freport\u002F20210527"]})
--- _nuxt/static/1645934143/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1645934143",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8ff5a385"}}}}(null,"\u002F"));
