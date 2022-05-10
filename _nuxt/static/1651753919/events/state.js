ec5c72250c56a684c84dc94a0309ced7fc947027
otkzh <otkzh@users.noreply.github.com>
2022-05-10 12:17:22 +0000

    deploy: f8c5993ca429dfde124c87e558fef2ade1c91eec

--- _nuxt/static/1651753919/events/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1651753919",layout:"default",error:a,serverRendered:true,routePath:"\u002Fevents",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"61e243d6"}}}}(null));
