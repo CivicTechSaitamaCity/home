acc08cf323e4a0ebf8581c284c6f53f6b94aab03
otkzh <otkzh@users.noreply.github.com>
2022-05-28 08:46:53 +0000

    deploy: f460d174cd1cddfbd3fac20d2b3f9b747179562b

---
+++ _nuxt/static/1653727610/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Freport","\u002Fevents","\u002Fnews","\u002Freport\u002F20211025","\u002Freport\u002F20210728","\u002Freport\u002F20210630","\u002Freport\u002F20210527","\u002Fproject\u002Fshindan","\u002Fproject\u002Fkyouzai","\u002Fproject\u002Ftable"]})
