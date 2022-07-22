fc4c89f31fb6b97549fd1ec62844d72d85dbb089
otkzh <otkzh@users.noreply.github.com>
2022-07-22 03:52:48 +0000

    deploy: 97ba76a4fd20ee57734ad2944b358299754ec8d0

--- _nuxt/static/1654923552/project/shindan/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1654923552",layout:"default",error:a,serverRendered:true,routePath:"\u002Fproject\u002Fshindan",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1afc208a"}}}}(null));
