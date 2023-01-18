af03c3af2109df2af3d578bf4f9ac865f681bf1e
Kazuho OHTA <otkzh77@gmail.com>
2023-01-18 10:54:58 +0900

    gtag設定

---
+++ plugins/vue-gtag.client.js
@@ -0,0 +1,10 @@
+// plugins/vue-gtag.client.js
+import VueGtag from 'vue-gtag-next'
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(VueGtag, {
+    property: {
+      id: 'G-QDNTQQJPTK',
+    }
+  })
+})
