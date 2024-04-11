d642f5b2e79b7cc27b60fb8670546303c9cd44fc
Kazuho OHTA <otkzh77@gmail.com>
2024-04-11 22:35:31 +0900

    Reportページ見栄え調整

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,5 +1,27 @@
 <template>
   <main>
+    <h2>Report</h2>
     <ContentDoc />
+    <nuxt-link to="/report">BACK</nuxt-link>
   </main>
 </template>
+<style lang="scss" scoped>
+h2 {
+  font-family: "Noto Sans JP", cursive;
+  width: 60%;
+  margin: 20px auto 20px;
+  font-size: 3rem;
+  font-weight: normal;
+  text-align: center;
+  background-size: 1px 1px;
+
+  a {
+    text-decoration: none;
+  }
+}
+main {
+  max-width: 1000px;
+  margin: auto;
+  // font-size: clamp(18px, 1vw, 24px);
+}
+</style>
--- pages/report.vue
+++ pages/report.vue
@@ -1,12 +1,38 @@
 <template>
-  <article>
-    <h1>Report</h1>
-    <nuxt-link v-for="(article, index) in reports" :key="index" :to="article._path">
-      <p>{{ article.title }}</p>
-    </nuxt-link>
-  </article>
+  <div>
+    <h2>Report</h2>
+    <article>
+      <nuxt-link
+        v-for="(article, index) in reports"
+        :key="index"
+        :to="article._path"
+      >
+        <p>{{ article.title }}</p>
+      </nuxt-link>
+    </article>
+  </div>
 </template>
 <script setup>
 const reports = await queryContent("/report").find();
 // console.log(reports)
 </script>
+<style lang="scss" scoped>
+h2 {
+  font-family: "Noto Sans JP", cursive;
+  width: 60%;
+  margin: 20px auto 20px;
+  font-size: 3rem;
+  font-weight: normal;
+  text-align: center;
+  background-size: 1px 1px;
+
+  a {
+    text-decoration: none;
+  }
+}
+article {
+  max-width: 1000px;
+  margin: auto;
+  // font-size: clamp(18px, 1vw, 24px);
+}
+</style>
