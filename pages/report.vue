c2e43abc1d7b1d73dabeabb9c51103780d5fe457
Kazuho Ohta <otkzh77@gmail.com>
2024-09-13 18:04:26 +0900

    report index 一旦戻す

--- pages/report.vue
+++ pages/report.vue
@@ -7,33 +7,14 @@
         :key="index"
         :to="article._path"
       >
-        <div class="report">
-          <div class="report-date">{{ date(article._path.slice(-8)) }}</div>
-          <div class="report-title">{{ article.title }}</div>
-        </div>
+        <p>{{ article.title }}</p>
       </nuxt-link>
     </article>
   </div>
 </template>
 <script setup>
-import { ref, onMounted } from "vue";
-
-const reports = ref([]);
-
-onMounted(async () => {
-  const fetchedReports = await queryContent("/report").find();
-  reports.value = fetchedReports.sort((a, b) => b._path.localeCompare(a._path));
-});
-
+const reports = await queryContent("/report").find();
 // console.log(reports)
-
-// 年月日の8桁の数字から日付を生成
-const date = (date) => {
-  const year = date.slice(0, 4);
-  const month = date.slice(4, 6);
-  const day = date.slice(6, 8);
-  return `${year}/${month}/${day}`;
-};
 </script>
 <style lang="scss" scoped>
 h2 {
@@ -53,18 +34,5 @@ article {
   max-width: 1000px;
   margin: auto;
   // font-size: clamp(18px, 1vw, 24px);
-}
-
-.report {
-  display: flex;
-  padding: 10px 0;
-  border-bottom: 1px solid #ccc;
-  &-date {
-    font-size: 1rem;
-    padding-right: 16px;
-  }
-  &-title {
-    font-size: 1.4rem;
-  }
 }
 </style>
