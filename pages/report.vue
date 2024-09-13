d4b5d6d9aec8c5745f646385301193f49bd33202
Kazuho Ohta <otkzh77@gmail.com>
2024-09-14 08:39:01 +0900

    レポートタイトル取得

--- pages/report.vue
+++ pages/report.vue
@@ -7,7 +7,10 @@
         :key="index"
         :to="article._path"
       >
-        <p>{{ article.title }}</p>
+        <div class="report">
+          <div class="report-date">{{ formatDate(article.reportDate) }}</div>
+          <div class="report-title">{{ getH1Text(article) }}</div>
+        </div>
       </nuxt-link>
     </article>
   </div>
@@ -18,6 +21,17 @@ const reports = await queryContent("/data")
   .where({ reportDate: { $gt: new Date(2020) } })
   .find();
 // console.log(reports)
+
+/**
+ * 記事から h1 要素のテキストを取得する関数
+ * @param {Object} article - コンテンツ記事オブジェクト
+ * @returns {String} h1 のテキストまたはフォールバックとして article.title
+ */
+
+const getH1Text = (article) => {
+  const h1 = article.body.children.find((child) => child.tag === "h1");
+  return h1 ? h1.children[0].value : article.title;
+};
 </script>
 <style lang="scss" scoped>
 h2 {
@@ -37,5 +51,17 @@ article {
   max-width: 1000px;
   margin: auto;
   // font-size: clamp(18px, 1vw, 24px);
+}
+.report {
+  display: flex;
+  padding: 10px 0;
+  border-bottom: 1px solid #ccc;
+  &-date {
+    font-size: 1rem;
+    padding-right: 16px;
+  }
+  &-title {
+    font-size: 1.2rem;
+  }
 }
 </style>
