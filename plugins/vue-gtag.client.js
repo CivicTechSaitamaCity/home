501dd3c219d1a851177027e5aafc7fc4c9657413
Kazuho OHTA <otkzh77@gmail.com>
2025-09-19 10:21:11 +0900

    chore(analytics): add debug logs to vue-gtag plugin for local verification

--- plugins/vue-gtag.client.js
+++ plugins/vue-gtag.client.js
@@ -2,9 +2,20 @@
 import VueGtag from 'vue-gtag-next'
 
 export default defineNuxtPlugin((nuxtApp) => {
+  // Initialize vue-gtag-next
   nuxtApp.vueApp.use(VueGtag, {
     property: {
       id: 'G-QDNTQQJPTK',
     }
   })
+
+  // Debug logging to help verify analytics during development
+  if (process.client) {
+    console.log('[vue-gtag] initialized (client)')
+
+    nuxtApp.hook('page:finish', (context) => {
+      // page:finish runs after navigation is complete
+      console.log('[vue-gtag] page:finish', context?.route?.path || '')
+    })
+  }
 })
