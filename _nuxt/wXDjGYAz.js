d5cedcac12d5d16b0fb0648e41254b0b24831aff
otkzh <otkzh@users.noreply.github.com>
2026-04-22 15:18:35 +0000

    deploy: d5724c17c2a38e300e002f8d3993e7fdffd66c66

---
+++ _nuxt/wXDjGYAz.js
@@ -0,0 +1,1 @@
+import{_ as s,o,c as r,Y as n}from"./Bg6_MuRv.js";const t={};function c(e,a){return o(),r("ul",null,[n(e.$slots,"default")])}const _=Object.assign(s(t,[["render",c]]),{__name:"ProseUl"});export{_ as default};
